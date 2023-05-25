3284a436d591b2d7494aab6f6a851c2c573aae7c
Valentin SUDRES <valentin.sudres@gmail.com>
2023-05-25 15:44:13 +0200

    Icones pour les tabs

--- ReactNativeProject/src/navigation/AppNavigator.js
+++ ReactNativeProject/src/navigation/AppNavigator.js
@@ -1,6 +1,10 @@
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {NavigationContainer} from '@react-navigation/native';
+
 import * as React from 'react';
+
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 import FoodDatabaseScreen from '../screens/FoodDatabaseScreen.js';
 import HealthGoalsScreen from '../screens/HealthGoalsScreen.js';
@@ -12,9 +16,45 @@ export default function AppNavigator() {
   return (
     <NavigationContainer>
       <Tab.Navigator>
-        <Tab.Screen name="Health Goals" component={HealthGoalsScreen} />
-        <Tab.Screen name="Food Database" component={FoodDatabaseScreen} />
-        <Tab.Screen name="Meals Planning" component={MealsPlanningScreen} />
+        <Tab.Screen
+          name="Health Goals"
+          component={HealthGoalsScreen}
+          options={{
+            tabBarIcon: ({color, size}) => (
+              <MaterialCommunityIcons
+                name="flag-checkered"
+                color={color}
+                size={size}
+              />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Food Database"
+          component={FoodDatabaseScreen}
+          options={{
+            tabBarIcon: ({color, size}) => (
+              <MaterialCommunityIcons
+                name="food-apple"
+                color={color}
+                size={size}
+              />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Meals Planning"
+          component={MealsPlanningScreen}
+          options={{
+            tabBarIcon: ({color, size}) => (
+              <MaterialCommunityIcons
+                name="food-turkey"
+                color={color}
+                size={size}
+              />
+            ),
+          }}
+        />
       </Tab.Navigator>
     </NavigationContainer>
   );
