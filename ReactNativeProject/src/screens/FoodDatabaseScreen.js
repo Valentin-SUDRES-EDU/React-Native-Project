4786553ab67db4fc495f516bfc559aeb5aed3c77
Aurélie PINNA <aurelie.pinna@edu.esiee.fr>
2023-05-26 14:34:41 +0200

    Updating FoodDataBase v2

--- ReactNativeProject/src/screens/FoodDatabaseScreen.js
+++ ReactNativeProject/src/screens/FoodDatabaseScreen.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import {
   View,
   Text,
@@ -6,6 +6,8 @@ import {
   StyleSheet,
   TouchableWithoutFeedback,
   Keyboard,
+  Image,
+  TouchableOpacity,
 } from 'react-native';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
@@ -15,17 +17,56 @@ const dismissKeyboard = () => {
 
 const FoodDatabaseScreen = () => {
   const [search, setSearch] = useState();
+  const [ingredient, setIngredient] = useState(null);
+
+  const fetchData = async () => {
+    try {
+      const API_ENDPOINT = 'https://api.edamam.com/api/food-database/v2/parser';
+      const API_KEY = '7a6626c0cc8f1ce64be0652e414a6fda';
+      const API_ID = 'a21c5bfa';
+      const query = search;
+
+      console.log(query);
+
+      const url = `${API_ENDPOINT}?ingr=${query}&app_id=${API_ID}&app_key=${API_KEY}`;
+
+      const response = await fetch(url);
+      const data = await response.json();
+
+      if (data.hints && data.hints.length > 0 && data.hints[0].food) {
+        setIngredient(data.hints[0].food);
+      } else {
+        setIngredient(null);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
     <TouchableWithoutFeedback onPress={dismissKeyboard}>
       <View style={styles.container}>
-        <View>
-          <Text style={styles.text}>Search your food items</Text>
-        </View>
+        <Text style={styles.text}>Search your food items</Text>
 
         <View style={styles.searchBar}>
-          <MaterialCommunityIcons name="magnify" size={40} color="navy" style={styles.icon} />
           <TextInput onChangeText={setSearch} value={search} style={styles.input} />
+
+          <TouchableOpacity style={styles.button} onPress={fetchData}>
+            <MaterialCommunityIcons name="magnify" size={40} color="white" style={styles.icon} />
+          </TouchableOpacity>
+        </View>
+
+        <View>
+          {ingredient && (
+            <View>
+              <Text>{ingredient.label}</Text>
+              <Image source={{ uri: ingredient.image }} style={{ width: 100, height: 100 }} />
+              <Text>{ingredient.nutrients.ENERC_KCAL} kcal</Text>
+              <Text>Fat : {ingredient.nutrients.FAT} g</Text>
+              <Text>Sugar : {ingredient.nutrients.SUGAR}</Text>
+              <Text>Carbohydrate : {ingredient.nutrients.CHOCDF} g</Text>
+            </View>
+          )}
         </View>
       </View>
     </TouchableWithoutFeedback>
@@ -33,18 +74,18 @@ const FoodDatabaseScreen = () => {
 };
 
 const styles = StyleSheet.create({
-  container:{
+  container: {
     flex: 1,
   },
 
   input: {
     height: 40,
     margin: 20,
-    borderWidth: 2,
+    borderWidth: 1,
     borderRadius: 20,
-    borderColor: 'navy',
+    borderColor: '#424B54',
     cursorColor: 'black',
-    backgroundColor: '#e8e8e8',
+    backgroundColor: 'white',
     padding: 10,
     width: '70%',
     alignItems: 'center',
@@ -66,9 +107,14 @@ const styles = StyleSheet.create({
   },
 
   icon: {
-    alignSelf: 'center',
-    marginLeft: 20,
   },
+
+  button:{
+    flexDirection: 'row', 
+    alignSelf: 'center',
+    borderRadius : 50,
+    backgroundColor: '#424B54',
+  }
 });
 
 export default FoodDatabaseScreen;
