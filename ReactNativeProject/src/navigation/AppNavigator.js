24eb573384e7eafc9890c99960275cc481c71e78
Aurélie PINNA <aurelie.pinna@edu.esiee.fr>
2023-05-26 17:09:24 +0200

    Upgrading headers colors

--- ReactNativeProject/src/navigation/AppNavigator.js
+++ ReactNativeProject/src/navigation/AppNavigator.js
@@ -17,28 +17,48 @@ export default function AppNavigator() {
           name="Health Goals"
           component={HealthGoalsScreen}
           options={{
+            headerStyle: {
+              backgroundColor: '#1D8EE4',
+            },
+            headerTintColor: '#fff',
             tabBarIcon: ({ color, size }) => (
               <MaterialCommunityIcons name="flag-checkered" color={color} size={size} />
             ),
+            tabBarActiveTintColor: '#1D8EE4',
+            tabBarInactiveTintColor: '#424B54',
           }}
         />
         <Tab.Screen
           name="Food Database"
           component={FoodDatabaseScreen}
           options={{
+            headerStyle: {
+              backgroundColor: '#3AB800',
+            },
+            headerTintColor: '#fff',
             tabBarIcon: ({ color, size }) => (
               <MaterialCommunityIcons name="food-apple" color={color} size={size} />
             ),
+            tabBarActiveTintColor: '#3AB800',
+            tabBarInactiveTintColor: '#424B54',
           }}
         />
         <Tab.Screen
           name="Meals Planning"
           component={MealsPlanningScreen}
           options={{
+            headerStyle: {
+              backgroundColor: '#E69035',
+            },
+            headerTintColor: '#fff',
             tabBarIcon: ({ color, size }) => (
               <MaterialCommunityIcons name="food-turkey" color={color} size={size} />
             ),
+            tabBarActiveTintColor: '#E69035',
+            tabBarInactiveTintColor: '#424B54',
           }}
+
+          
         />
       </Tab.Navigator>
     </NavigationContainer>
