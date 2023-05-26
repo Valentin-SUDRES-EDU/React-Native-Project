529923efc6961c2ef6b9f6204d99520b87f874cc
Aurélie PINNA <aurelie.pinna@edu.esiee.fr>
2023-05-26 11:20:15 +0200

    Updating Screen FoodDatabase

--- ReactNativeProject/src/screens/FoodDatabaseScreen.js
+++ ReactNativeProject/src/screens/FoodDatabaseScreen.js
@@ -1,12 +1,74 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import { useState } from 'react';
+import {
+  View,
+  Text,
+  TextInput,
+  StyleSheet,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from 'react-native';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
+const dismissKeyboard = () => {
+  Keyboard.dismiss();
+};
 
 const FoodDatabaseScreen = () => {
+  const [search, setSearch] = useState();
+
   return (
-    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>Food Database!</Text>
-    </View>
+    <TouchableWithoutFeedback onPress={dismissKeyboard}>
+      <View style={styles.container}>
+        <View>
+          <Text style={styles.text}>Search your food items</Text>
+        </View>
+
+        <View style={styles.searchBar}>
+          <MaterialCommunityIcons name="magnify" size={40} color="navy" style={styles.icon} />
+          <TextInput onChangeText={setSearch} value={search} style={styles.input} />
+        </View>
+      </View>
+    </TouchableWithoutFeedback>
   );
 };
+
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+  },
+
+  input: {
+    height: 40,
+    margin: 20,
+    borderWidth: 2,
+    borderRadius: 20,
+    borderColor: 'navy',
+    cursorColor: 'black',
+    backgroundColor: '#e8e8e8',
+    padding: 10,
+    width: '70%',
+    alignItems: 'center',
+    alignSelf: 'center',
+  },
+
+  text: {
+    width: '100%',
+    margin: 20,
+    textAlign: 'center',
+    fontSize: 30,
+    alignItems: 'center',
+    alignSelf: 'center',
+  },
+
+  searchBar: {
+    display: 'flex',
+    flexDirection: 'row',
+  },
+
+  icon: {
+    alignSelf: 'center',
+    marginLeft: 20,
+  },
+});
 
 export default FoodDatabaseScreen;
